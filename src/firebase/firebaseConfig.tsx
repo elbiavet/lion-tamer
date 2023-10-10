2e878a94d25c5ebb72fd436bfc0d43e580e351cc
Elbia <elbia2@hotmail.com>
2023-10-10 13:33:11 +0200

    modificar firebaseConfig

--- src/firebase/firebaseConfig.tsx
+++ src/firebase/firebaseConfig.tsx
@@ -2,25 +2,27 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite"
-import { getEnvironments } from "../helpers/getEnvironments";
+// import { getEnvironments } from "../helpers/getEnvironments";
 
-const {
-    VITE_APIKEY,
-    VITE_AUTHDOMAIN,
-    VITE_PROJECTID,
-    VITE_STORAGEBUCKET,
-    VITE_MESSAGINGSENDERID,
-    VITE_APPID,
-  } = getEnvironments();
+// const {
+//     VITE_APIKEY,
+//     VITE_AUTHDOMAIN,
+//     VITE_PROJECTID,
+//     VITE_STORAGEBUCKET,
+//     VITE_MESSAGINGSENDERID,
+//     VITE_APPID,
+//   } = getEnvironments();
 
-  // dev/prod
+
+
+// dev/prod
 const firebaseConfig = {
-    apiKey: VITE_APIKEY,
-    authDomain: VITE_AUTHDOMAIN,
-    projectId: VITE_PROJECTID,
-    storageBucket: VITE_STORAGEBUCKET,
-    messagingSenderId: VITE_MESSAGINGSENDERID,
-    appId: VITE_APPID 
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID 
   }; 
 
 export const FirebaseApp = initializeApp(firebaseConfig);
